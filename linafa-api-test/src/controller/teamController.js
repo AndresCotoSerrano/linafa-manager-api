62869988fc4d1cf2818cfec3d87e5ef3d4e9d34d
Andres Coto <andres.coto3539@gmail.com>
2019-01-18 14:52:14 -0600

    delete de team

--- linafa-api-test/src/controller/teamController.js
+++ linafa-api-test/src/controller/teamController.js
@@ -2,14 +2,27 @@ const mysqlConnection = require('../db/database')
 
 module.exports = {
     insertTeams: (req, res) => {
-        const {name_team,id_region,id_division,id_group} = req.body;
+        const { name_team, id_region, id_division, id_group } = req.body;
         let procedure = 'call add_teams(?,?,?,?)'
-        mysqlConnection.query(procedure,[name_team,id_region,id_division,id_group], (err, rows, fields) => {
+        mysqlConnection.query(procedure, [name_team, id_region, id_division, id_group], (err, rows, fields) => {
             if (!err) {
                 res.json(rows[0]);
             } else {
                 // res.json(rows[0]);
                 console.log('error')
+            }
+        });
+    },
+    deleteTeams: (req, res) => {
+        const { p_name_team } = req.body;
+        let procedure = 'call delete_team(?)'
+        mysqlConnection.query(procedure, [p_name_team], (err, rows, fields) => {
+            if(!err){
+                res.json(rows[0]);
+            }else{
+                res.json({
+                    "msg":"equipo no existe"
+                })
             }
         });
     }
