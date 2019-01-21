da2e67d387c704bcd3ac6b6c9a34ea9921f2ffaf
Andres Coto <andres.coto3539@gmail.com>
2019-01-21 12:02:00 -0600

    detalles

--- linafa-api-test/src/controller/teamController.js
+++ linafa-api-test/src/controller/teamController.js
@@ -2,9 +2,11 @@ const mysqlConnection = require('../db/database')
 
 module.exports = {
     insertTeams: (req, res) => {
-        const { name_team, id_region, id_division, id_group } = req.body;
+        const { name_team, region, division, group } = req.body;
         let procedure = 'call add_teams(?,?,?,?)'
-        mysqlConnection.query(procedure, [name_team, id_region, id_division, id_group], (err, rows, fields) => {
+        console.log(req.body)
+        console.log(name_team + region + division + group);
+        mysqlConnection.query(procedure, [name_team, region, division, group], (err, rows, fields) => {
             if (!err) {
                 res.json(rows[0]);
             } else {
