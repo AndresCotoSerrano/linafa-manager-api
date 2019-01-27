3d6b6506a92d4632d4952a17d0fbc6eae5c154f2
Andres Coto <andres.coto3539@gmail.com>
2019-01-27 17:58:30 -0600

    cambios a post de los get

--- linafa-api-test/src/routes/users.js
+++ linafa-api-test/src/routes/users.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-
 //connection who brings data
 const mysqlConnection = require('../db/database');
 
 //consulta
-router.get('/', (req, res) => {
-
-    mysqlConnection.query('call all_users()', (err, rows, fields) => {
+router.post('/getUsers', (req, res) => {
+    let { key } = req.body;
+    mysqlConnection.query('call all_users(?)', [key], (err, rows, fields) => {
 
         if (!err) {
 
-            res.json(rows);
+            res.json(rows[0]);
             console.log(rows);
         } else {
 
@@ -22,30 +21,16 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id', (req, res) => {
-
-    const { id } = req.params;
-    mysqlConnection.query('SELECT * FROM user_programa WHERE id = ?', [id]
-        , (err, rows, fields) => {
-            if (!err) {
-                res.json(rows[0]);
-            } else {
-                console.log(err);
-            }
-
-        });
-
-});
 
 router.post('/', (req, res) => {
-const {id,user_name,pass_login,id_region,phone_number,id_programa,role} = req.body;
+    const { id, user_name, pass_login, id_region, phone_number, id_programa, role } = req.body;
     const query = `
 CALL addEditUsers(?, ?, ?, ?, ?, ?, ?);
 `;
 
-    mysqlConnection.query(query,[id,user_name,pass_login,id_region,phone_number,id_programa,role],(err,rows,fields)=>{
+    mysqlConnection.query(query, [id, user_name, pass_login, id_region, phone_number, id_programa, role], (err, rows, fields) => {
         if (!err) {
-            res.json({status:'user saved'});
+            res.json({ status: 'user saved' });
         } else {
             console.log(err);
         }
